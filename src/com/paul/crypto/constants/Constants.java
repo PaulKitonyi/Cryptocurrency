df69f15f309edfabc6a5b6ee134ba89d35b8be6f
Paul Kitonyi <paulmucimah@gmail.com>
2020-01-23 09:47:43 +0300

    add constants

---
+++ src/com/paul/crypto/constants/Constants.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.paul.crypto.constants;
+
+/**
+ *
+ * @author h4ck3r
+ */
+public class Constants {
+    
+    private Constants(){}
+    
+    public static final int DIFFICULTY = 5;
+    public static final String GENESIS_PREV_HASH = "00000000000000000000000000000000000000000000000000000000000000000";
+    public static final double MINER_REWARD = 12.5;
+    
+}
